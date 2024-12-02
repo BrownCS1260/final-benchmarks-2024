9adde55de270e6e48ad6cf746351dff970d74df7
Patrick Peng <44712940+tenchipsofsalt@users.noreply.github.com>
2024-12-02 18:40:14 -0500

    Update tenchipsofsalt-log.lisp

--- benchmarks/tenchipsofsalt-log.lisp
+++ benchmarks/tenchipsofsalt-log.lisp
@@ -1,5 +1,5 @@
 (define (div x y) (if (< x y) 0 (+ 1 (div (- x y) y))))
 
-(define (log n x d) (if (= n 0) x (log (div n d) (+ 1 x))))
+(define (log n x d) (if (= n 0) x (log (div n d) (+ 1 x) d)))
 
 (log 65536 0 2)
