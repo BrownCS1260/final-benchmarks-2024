669c9765eb24bea1c7f62c6994efdea804a9e5d1
Alexander Cueva <117316944+kerumeto@users.noreply.github.com>
2024-12-02 20:14:34 +0000

    fixes

--- benchmarks/constantIn.lisp
+++ benchmarks/constantIn.lisp
@@ -1,3 +1,3 @@
 (define (f x) (+ x (+ x (+ x (+ 3 (- x (- x x)))))))
 
-(print (lambda ((x 10)) (f x)))
+(print (let ((x 10)) (f x)))
--- benchmarks/inlining.lisp
+++ benchmarks/inlining.lisp
@@ -1,6 +1,6 @@
 (define (f x) (+ x 2))
 (define (g y) 
-    (lambda ((z (- y 5)))
+    (let ((z (- y 5)))
         (+ (f z) (+ (f y) (f (+ z 10))))
     )
 )
