c1a8749f22efb4e545615a882abe1403f66db59b
jean-yoo <93178936+jean-yoo@users.noreply.github.com>
2024-12-03 18:31:13 +0000

    Format

--- benchmarks/cprop-let-if-add.lisp
+++ benchmarks/cprop-let-if-add.lisp
@@ -1,7 +1,6 @@
-(let ((a (let ((x 5)) (add1 x))))
-  (let ((c (if (= a 6)
+(let (a (let ((x 5)) (add1 x)))
+  (let (c (if (= a 6)
                (+ a b)
-               (- a a))))
-    (let ((e (let ((z (+ c d))) (add1 z))))
+               (- a a)))
+    (let (e (let ((z (+ c d))) (add1 z)))
       (print (+ a (+ c e))))))
-      
