cd6a9e3690105846f728d0c6a1f68d2e380bd50f
Jaideep <123285085+jnaik2@users.noreply.github.com>
2024-12-03 14:36:09 -0500

    Update constant-propagation2.lisp

--- benchmarks/constant-propagation2.lisp
+++ benchmarks/constant-propagation2.lisp
@@ -1,1 +1,1 @@
-(let ((x (add1 0)) (y (+ 1 1))) (print (if (= (add1 x) (sub1 y)) (add1 (sub1 x)) (sub1 (add1 y)))))
+(let ((x (add1 0))) (let ((y (+ 1 1))) (if (= (add1 x) (sub1 y)) (add1 (sub1 x)) (sub1 (add1 y)))))
