f304905732074db914c3d41bd63a23411e6ac907
Jerry Xia <97870693+jxia3@users.noreply.github.com>
2024-12-02 19:37:51 -0500

    add file

---
+++ benchmarks/const_do.lisp
@@ -0,0 +1,310 @@
+(define (calc value)
+    (do
+        20
+        false
+        false
+        true
+        -29
+        false
+        (if (left (pair false false)) 34 false)
+        65
+        false
+        ()
+        80
+        -2
+        96
+        56
+        12
+        true
+        -59
+        -40
+        (if (right (pair false false)) 37 77)
+        31
+        18
+        86
+        false
+        -36
+        true
+        90
+        -91
+        -95
+        (if (right (pair true true)) true false)
+        false
+        -41
+        (if (left (pair true true)) 38 40)
+        ()
+        (if (left (pair true true)) true -91)
+        -42
+        ()
+        ()
+        -49
+        true
+        (if (right (pair true true)) true true)
+        24
+        18
+        true
+        77
+        (if (right (pair true true)) 23 40)
+        -91
+        true
+        -21
+        80
+        true
+        (if (right (pair false false)) true false)
+        -84
+        (if (right (pair false false)) 53 40)
+        true
+        true
+        -94
+        80
+        false
+        -67
+        21
+        true
+        false
+        false
+        -49
+        (if (left (pair true true)) 69 25)
+        (left (pair false 51))
+        38
+        true
+        ()
+        (if (right (pair false false)) true 94)
+        ()
+        ()
+        true
+        ()
+        true
+        -46
+        false
+        -86
+        ()
+        70
+        -46
+        62
+        false
+        false
+        65
+        6
+        true
+        29
+        -9
+        ()
+        -29
+        74
+        false
+        88
+        -26
+        true
+        -7
+        -90
+        ()
+        true
+        true
+        -59
+        (if (right (pair true true)) 91 -58)
+        false
+        false
+        8
+        40
+        true
+        ()
+        -85
+        10
+        38
+        -7
+        true
+        -8
+        -58
+        3
+        (if (left (pair true true)) 45 false)
+        14
+        true
+        4
+        true
+        ()
+        75
+        (if (right (pair true true)) true false)
+        false
+        -4
+        -74
+        55
+        87
+        (if (right (pair false false)) () -60)
+        false
+        14
+        true
+        10
+        true
+        (if (left (pair true true)) 71 true)
+        true
+        7
+        false
+        94
+        87
+        true
+        51
+        true
+        true
+        (if (right (pair false false)) -1 -70)
+        (if (left (pair false false)) true 60)
+        true
+        ()
+        36
+        24
+        -31
+        (if (right (pair true true)) -1 33)
+        ()
+        25
+        49
+        -73
+        65
+        2
+        false
+        (if (left (pair false false)) false 37)
+        true
+        25
+        95
+        -19
+        70
+        -3
+        67
+        (if (right (pair false false)) -74 -62)
+        (if (right (pair true true)) -28 -23)
+        -8
+        false
+        true
+        32
+        false
+        -11
+        -55
+        ()
+        ()
+        98
+        3
+        49
+        -55
+        -54
+        -69
+        -37
+        77
+        28
+        20
+        -16
+        true
+        -62
+        ()
+        false
+        40
+        false
+        (if (right (pair false false)) -42 99)
+        -82
+        -56
+        17
+        false
+        72
+        false
+        -35
+        54
+        98
+        -73
+        false
+        -39
+        85
+        (if (left (pair true true)) true true)
+        -29
+        54
+        true
+        (if (left (pair false false)) true false)
+        false
+        false
+        false
+        -15
+        -31
+        true
+        13
+        (if (right (pair true true)) -8 -38)
+        false
+        -73
+        (if (left (pair true true)) 13 ())
+        55
+        true
+        true
+        (if (right (pair false false)) 8 95)
+        false
+        84
+        9
+        89
+        34
+        true
+        -63
+        ()
+        75
+        9
+        true
+        38
+        76
+        true
+        26
+        -63
+        21
+        (if (right (pair true true)) false -99)
+        -21
+        79
+        68
+        false
+        63
+        false
+        -38
+        69
+        false
+        true
+        false
+        false
+        ()
+        (if (left (pair true true)) 12 -90)
+        ()
+        -77
+        86
+        false
+        -34
+        -1
+        ()
+        77
+        false
+        34
+        true
+        true
+        false
+        31
+        97
+        (right (pair -42 ()))
+        81
+        65
+        -57
+        true
+        false
+        -43
+        74
+        (if (left (pair false false)) true 37)
+        10
+        false
+        -8
+        -19
+        false
+        true
+        (if (left (pair true true)) 23 -37)
+        -3
+        true
+        50
+        -48
+        -64
+        -15
+        15
+        (left (pair value ()))))
+(print
+    (+ (calc 1)
+        (+ (calc 2)
+            (+ (calc 3)
+                (+ (calc 4)
+                    (+ (calc 5) (calc 6)))))))
