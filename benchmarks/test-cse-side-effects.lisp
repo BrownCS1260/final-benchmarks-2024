7b5cc68f1d849e93b6b6ce1d02e17870ccf6c0cf
brandon-gong <brandon.gong@brandongong.org>
2024-12-03 19:14:07 -0500

    fix test

--- benchmarks/test-cse-side-effects.lisp
+++ benchmarks/test-cse-side-effects.lisp
@@ -1,6 +1,6 @@
-(function (p x) (print (+ 2 x)))
-(function (q x) (+ 2 x))
+(define (p x) (print (+ 2 x)))
+(define (q x) (+ 2 x))
 
 (do
   (let ((x 3)) (do (p x) (p x) (p x)))
-	(let ((x 4)) (do (p (q x)) (p (+ 10 (q x)) (p (if (< 3 4) (q x) (q x)))))))
+	(let ((x 4)) (do (p (q x)) (p (+ 10 (q x))) (p (if (< 3 4) (q x) (q x))))))
