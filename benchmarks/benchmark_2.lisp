1822d59a315da91bd053074f34e53cf021a49e78
Rob Lewis <rob.y.lewis@gmail.com>
2024-12-05 15:16:24 -0500

    no mul

--- benchmarks/benchmark_2.lisp
+++ benchmarks/benchmark_2.lisp
@@ -1,4 +1,4 @@
 (define (cube x)
-	(*x (* x x)))
+	(+x (+ x x)))
 
 (do (print (let ((x (read-num))) (+ (let ((y (+ 3 5))) (cube x))))))
--- benchmarks/lchoi-unroll.lisp
+++ benchmarks/lchoi-unroll.lisp
@@ -1,4 +1,4 @@
 (define (leftshift num bits)
-    (if (< bits 1) num (* 2 (leftshift num (- bits 1)))))
+    (if (< bits 1) num (+ (leftshift num (- bits 1)) (leftshift num (- bits 1)))))
 
 (print (leftshift 1 11))
