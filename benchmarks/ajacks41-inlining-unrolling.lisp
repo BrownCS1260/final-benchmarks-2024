a1adae8d2870feb5d9bc7f41e1295a2ab0dcd144
Alejandro Jackson <84302777+alejack312@users.noreply.github.com>
2024-12-04 23:36:07 -0500

    Update ajacks41-inlining-unrolling.lisp

--- benchmarks/ajacks41-inlining-unrolling.lisp
+++ benchmarks/ajacks41-inlining-unrolling.lisp
@@ -1,3 +1,6 @@
-(define (add_pair_elements p) (+ (left p) (right p)))
-(define (sum n acc) (if (= n 0) acc (sum (sub1 n) (+ acc n))))
-(print (+ (add_pair_elements (pair 5 5)) (sum 3 0)))
+(define (pairadd p) (+ (left p) (right p)))
+(define (sum n acc)
+    (if (= n 0)
+        acc
+        (sum (- n 1) (+ acc n))))
+(print (+ (pairadd (pair 5 5)) (sum 3 0)))
