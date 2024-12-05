985b69d6e7b77e1ce1a360665dc2a5aac67cf592
Rob Lewis <rob.y.lewis@gmail.com>
2024-12-05 15:31:06 -0500

    fix

--- benchmarks/lchoi-prop.lisp
+++ benchmarks/lchoi-prop.lisp
@@ -1,4 +1,4 @@
-(let ((x (- 5 4))
-    (let ((y (+ x 4))
-        (let ((z (+ x y))
-            (print (+ x (+ y z)))))))))
+(let ((x (- 5 4)))
+    (let ((y (+ x 4)))
+        (let ((z (+ x y)))
+            (print (+ x (+ y z))))))
