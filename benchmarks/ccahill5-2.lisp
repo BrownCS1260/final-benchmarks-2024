e5f01ce41913daeb3f4c3ee22f620fa49b6fb7f5
caroline-cahill <113068697+caroline-cahill@users.noreply.github.com>
2024-12-05 04:50:30 +0000

    third test

--- benchmarks/ccahill5-2.lisp
+++ benchmarks/ccahill5-2.lisp
@@ -1,8 +1,8 @@
 (let ((a (readnum)))
     (let ((b (readnum)))
         (if (= 0 (- a b))
-            true
-            false
+            (print true)
+            (print false)
         )
     )
 )
