2d462846554790aa8c8f35cc36987113638d02bd
adithya-1-4 <118315098+adithya-1-4@users.noreply.github.com>
2024-12-10 05:04:04 +0000

    minor change

--- benchmarks/asrira17-3.lisp
+++ benchmarks/asrira17-3.lisp
@@ -1,7 +1,9 @@
+(define (even n) (if (zero? n) true (odd (sub1 n))))
+(define (odd n) (if (zero? n) false (not (even n))))
 (define (filter-even l)
   (if (empty? l)
       ()
-      (if (even? (left l))
+      (if (even (left l))
           (pair (left l) (filter-even (right l)))
           (filter-even (right l)))))
 
